58d73eb0b0fd5414cbd22313cc9973b9645a5981
zhangcheng <52xinyun>
2022-10-17 16:59:26 +0800

    update

--- src/js/_enemy.js
+++ src/js/_enemy.js
@@ -10,7 +10,7 @@ class Enemy{
         // this.enemy =  new PIXI.Text(name);
         this.enemy.position.x = x;
         this.enemy.position.y = y;
-        this.enemy.vy = 2;
+        this.enemy.vy = 4;
         this.enemy.vx = 0;
         this.enemy.coin = 2;
         this.enemy.health = 2;
